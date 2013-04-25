1cc761f12fff431f792c845a2f18d0858ebe3cba
etosch <etosch@gmail.com>
2013-04-25 17:45:17 -0400

    fixed weird problem

--- src/survey/questionnaire.py
+++ src/survey/questionnaire.py
@@ -73,30 +73,25 @@ class Question :
             val = val + "\t" + str(i) + ". " + str(self.options[i]) + "\n"
         return val
 
-if __name__=="__main__":
+q1 = Question("What is your age?"
+              , ["< 18", "18-34", "35-64", "> 65"]
+              , qtypes["radio"])              
 
-    q1 = Question("What is your age?"
-                  , ["< 18", "18-34", "35-64", "> 65"]
-                  , qtypes["radio"])              
+q2 = Question("What is your political affiliation?"
+              , ["Democrat", "Republican", "Indepedent"]
+              , qtypes["radio"]
+              , shuffle=True)
 
-    q2 = Question("What is your political affiliation?"
-                  , ["Democrat", "Republican", "Indepedent"]
-                  , qtypes["radio"]
-                  , shuffle=True)
+q3 = Question("Which issues do you care about the most?"
+              , ["Gun control", "Reproductive Rights", "The Economy", "Foreign Relations"]
+              , qtypes["check"]
+              ,shuffle=True)
 
-    q3 = Question("Which issues do you care about the most?"
-                   , ["Gun control", "Reproductive Rights", "The Economy", "Foreign Relations"]
-                   , qtypes["check"]
-                   ,shuffle=True)
+q4 = Question("What is your year of birth?"
+              , [x+1910 for x in range(80)]
+              , qtypes["dropdown"])
 
-    q4 = Question("What is your year of birth?"
-                   , [x+1910 for x in range(80)]
-                   , qtypes["dropdown"])
-                   
-    survey = Survey([q1, q2, q3, q4])
+survey = Survey([q1, q2, q3, q4])
 
+if __name__ == "__main__" : 
     survey.take_survey()
-
-
-               
-               
