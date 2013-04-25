b6410b5236e7570352116a710486eab878ed0b1f
etosch <etosch@gmail.com>
2013-04-25 15:24:42 -0400

    now with comments

--- src/survey/questionnaire.py
+++ src/survey/questionnaire.py
@@ -1,3 +1,14 @@
+# basic idea of the questionnaire is to provide the first pass of representing 
+# the survey
+
+# todo:
+# - get ride of makeblahblahblah -> we're not going to have classes for Radio, etc.
+# - create Survey class, which is a list of questions, where each instance shuffles 
+#   the questions themselves and if the questions have shuffle=True, shuffles the options.
+# - Survey needs a function to proffer the survey and get a response. This response
+#   should be returned as a some kind of collection of data containing the actual responses,
+#   the question ids (since they might be shuffled) and maybe a confidence or something
+
 from uuid import uuid1
 
 qtypes = {"freetext" : 0 , "radio" : 1 , "check" : 2 , "dropdown" : 3}
