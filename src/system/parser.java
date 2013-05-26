66d578c978ec271b102892e45c1005850692c59d
Jessica Newman <jnewman@cs.umass.edu>
2013-05-26 18:17:44 -0400

    Parser can completely parse input file. 'process' function creates variables for each of the columns. Will probably call createHTML or something from here for the demo. Stuff that doesn't work yet: Newlines within questions. I will find some way to fix the regex to handle this soon. Anyway, for now I'll be working on creating some HTML.

--- src/system/parser.java
+++ src/system/parser.java
@@ -1,12 +1,14 @@
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
 
 public class parser
 {
-    String regex = "\\s*\"([^\"]*)\"|\\s*\\[\\[([^\\[\\]]*)\\]\\]\\s*|\\s*\\[([^\\[\\]]*)\\]|\\s*(?<=,|^)([^,]*)(?:,|$)";
+    String regex = "\\s*\"([^\"]*)\"|\\s*\\[\\[([^\\[\\]]*)\\]\\]\\s*|\\s*\\[([^\\[\\]]*)\\]|(?<=, ?|^)([^,]*)(?:, ?|$)";
     private final Pattern csvPattern = Pattern.compile(regex);
     private ArrayList<String> allMatches = null;
     private Matcher matcher = null;
@@ -67,11 +69,80 @@ public class parser
         String[] lines = file.split("\\r?\\n");
         return lines;
     }
+
+    static void process(String[] parsedData)
+    {
+        int block = -1;
+        String questionText = "";
+        String resource = "";
+        List<String> options = new ArrayList<String>();
+        boolean exclusive=true, ordered=true, perturb=true;
+        if (parsedData.length < 1)
+        {
+            return;
+        }
+        if (parsedData.length > 0 && !parsedData[0].equals(""))
+        {
+            try
+            {
+                block = Integer.parseInt(parsedData[0]);
+            }
+            catch (java.lang.NumberFormatException e)
+            {
+                block = -1;
+            }
+        }
+        if (parsedData.length > 1 && !parsedData[1].equals(""))
+        {
+            questionText = parsedData[1];
+        }
+        if (parsedData.length > 2 && !parsedData[2].equals(""))
+        {
+            resource = parsedData[2];
+        }
+        if (parsedData.length > 3 && !parsedData[3].equals(""))
+        {
+            options = Arrays.asList(parsedData[3].split(",| ,"));
+        }
+        if (parsedData.length > 4 && !parsedData[4].equals(""))
+        {
+            if (parsedData[4].equalsIgnoreCase("no") || parsedData[4].equalsIgnoreCase("n"))
+            {
+                exclusive = false;
+            }
+        }
+        if (parsedData.length > 5 && !parsedData[5].equals(""))
+        {
+            if (parsedData[5].equalsIgnoreCase("no") || parsedData[5].equalsIgnoreCase("n"))
+            {
+                ordered = false;
+            }
+        }
+        if (parsedData.length > 6 && !parsedData[6].equals(""))
+        {
+            if (parsedData[6].equalsIgnoreCase("no") || parsedData[6].equalsIgnoreCase("n"))
+            {
+                perturb = false;
+            }
+        }
+        System.out.println("Block: " + block);
+        System.out.println("Question text: " + questionText);
+        System.out.println("Resource: " + resource);
+        System.out.println("Options:");
+        System.out.println(options);
+        System.out.println("Exclusive? " + exclusive);
+        System.out.println("Ordered? " + ordered);
+        System.out.println("Perturb? " + perturb);
+        System.out.println();
+        return;
+    }
     
     public static void main(String[] args)
     {
         String fileContents = "";
         String[] lines;
+        List<String> toQuestion = new ArrayList<String>();
+        String[] parsedData;
         if (args.length == 0)
         {
             System.out.println("Usage: java parser.java [file path]");
@@ -90,10 +161,25 @@ public class parser
         lines = csvParser.split(fileContents);
         for (int i = 0; i < lines.length; i++)
         {
-            System.out.println("Testing parser with " + lines[i]);
-            for (String s : csvParser.parse(lines[i]))
+            parsedData = csvParser.parse(lines[i]);
+            if (toQuestion.size() == 0 || (parsedData.length > 0 && !parsedData[1].equals(toQuestion.get(1))))
             {
-                System.out.println(s);
+                String[] q = toQuestion.toArray(new String[toQuestion.size()]);
+                System.out.println(toQuestion);
+                process(q);
+                toQuestion = Arrays.asList(parsedData);
+            }
+            else
+            {
+                if (toQuestion.size() > 3 && parsedData.length > 3)
+                {
+                    toQuestion.set(3, toQuestion.get(3) + "," + parsedData[3]);
+                }
+                else
+                {
+                    System.out.println("Data in wrong format. Check line " + i + 1 + " .");
+                    System.exit(0);
+                }
             }
         }
     }
