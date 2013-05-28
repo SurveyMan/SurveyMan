9da8d020ba33fd8277aec85710f9124d264f426f
Jessica Newman <jnewman@cs.umass.edu>
2013-05-27 21:28:24 -0400

    Forgot to mention you run with javac parser.java, etc.

--- src/system/parser.java
+++ src/system/parser.java
@@ -8,7 +8,7 @@ import java.io.File;
 
 public class parser
 {
-    String regex = "\\s*\"([^\"]*)\"|\\s*\\[\\[([^\\[\\]]*)\\]\\]\\s*|\\s*\\[([^\\[\\]]*)\\]|(?<=, ?|^)([^,]*)(?:, ?|$)";
+    String regex = "\\s*\"([^\"]*)\"|\\s*(\\[\\[[^\\[\\]]*\\]\\])\\s*|\\s*\\[([^\\[\\]]*)\\]|\\s?(?<=, ?|^)([^,]*)(?:, ?|$)";
     private final Pattern csvPattern = Pattern.compile(regex);
     private ArrayList<String> allMatches = null;
     private Matcher matcher = null;
@@ -69,79 +69,14 @@ public class parser
         String[] lines = file.split("\\r?\\n");
         return lines;
     }
-
-    static void process(String[] parsedData)
-    {
-        int block = -1;
-        String questionText = "";
-        String resource = "";
-        List<String> options = new ArrayList<String>();
-        boolean exclusive=true, ordered=true, perturb=true;
-        if (parsedData.length < 1)
-        {
-            return;
-        }
-        if (parsedData.length > 0 && !parsedData[0].equals(""))
-        {
-            try
-            {
-                block = Integer.parseInt(parsedData[0]);
-            }
-            catch (java.lang.NumberFormatException e)
-            {
-                block = -1;
-            }
-        }
-        if (parsedData.length > 1 && !parsedData[1].equals(""))
-        {
-            questionText = parsedData[1];
-        }
-        if (parsedData.length > 2 && !parsedData[2].equals(""))
-        {
-            resource = parsedData[2];
-        }
-        if (parsedData.length > 3 && !parsedData[3].equals(""))
-        {
-            options = Arrays.asList(parsedData[3].split(",| ,"));
-        }
-        if (parsedData.length > 4 && !parsedData[4].equals(""))
-        {
-            if (parsedData[4].equalsIgnoreCase("no") || parsedData[4].equalsIgnoreCase("n"))
-            {
-                exclusive = false;
-            }
-        }
-        if (parsedData.length > 5 && !parsedData[5].equals(""))
-        {
-            if (parsedData[5].equalsIgnoreCase("no") || parsedData[5].equalsIgnoreCase("n"))
-            {
-                ordered = false;
-            }
-        }
-        if (parsedData.length > 6 && !parsedData[6].equals(""))
-        {
-            if (parsedData[6].equalsIgnoreCase("no") || parsedData[6].equalsIgnoreCase("n"))
-            {
-                perturb = false;
-            }
-        }
-        System.out.println("Block: " + block);
-        System.out.println("Question text: " + questionText);
-        System.out.println("Resource: " + resource);
-        System.out.println("Options:");
-        System.out.println(options);
-        System.out.println("Exclusive? " + exclusive);
-        System.out.println("Ordered? " + ordered);
-        System.out.println("Perturb? " + perturb);
-        System.out.println();
-        return;
-    }
     
     public static void main(String[] args)
     {
+        webGenerator generator = new webGenerator();
         String fileContents = "";
         String[] lines;
         List<String> toQuestion = new ArrayList<String>();
+        ArrayList<String[]> allQuestions = new ArrayList<String[]>();
         String[] parsedData;
         if (args.length == 0)
         {
@@ -165,8 +100,10 @@ public class parser
             if (toQuestion.size() == 0 || (parsedData.length > 0 && !parsedData[1].equals(toQuestion.get(1))))
             {
                 String[] q = toQuestion.toArray(new String[toQuestion.size()]);
-                System.out.println(toQuestion);
-                process(q);
+                if (i != 0 && i != 1)
+                {
+                    allQuestions.add(q);
+                }
                 toQuestion = Arrays.asList(parsedData);
             }
             else
@@ -177,10 +114,11 @@ public class parser
                 }
                 else
                 {
-                    System.out.println("Data in wrong format. Check line " + i + 1 + " .");
+                    System.out.println("Data in wrong format. Check line " + (i + 1) + " .");
                     System.exit(0);
                 }
             }
         }
+        generator.generateSurvey(allQuestions);
     }
 }
