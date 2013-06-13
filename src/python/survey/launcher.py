b768449eebe6e1355b88bfd4b5a1ae60bd81794a
etosch <etosch@gmail.com>
2013-06-13 11:24:59 -0400

    last round of reorganization for now

---
+++ src/python/survey/launcher.py
@@ -0,0 +1,248 @@
+from questionnaire import *
+import json, uuid, pickle, sys, os
+import numpy as np
+
+__doc__ = """Execute launcher by calling : python launcher.py arg1=val1 arg2=val2 ...args are optional. Valid args include:
+\t- display : a boolean indicating whether a graphical display of relevant statistics should be shown
+\t- simulation : a path indicating the file to load that contains data for simulating a survey. See simulation.py for an example
+\t- file : a path indicating the file containing a json representation of the survey to load
+\t- stop : name of the stop condition to be used. Should either be a stop condition defined in launcher (references held in the global dictionary stop_dict) or one defined in the simulation file.
+\t- outdir : The destination directory for computed data. Default is the current directory.
+\t- outformat : The name of the function used to format output. Output includes the dictionaries of questions, options, the counts, and outliers. Defaults to str. This module imports pickle, so pickle.dump is also an option.
+\t- responsefn : The name of the function used to get responses. If running a simulation, this should be the name of the appropriate function defined in the simulation file. If running in live mode, it defaults to a crowsourcing connector (not yet implemented).
+"""
+
+# quid_dict = {QUID, question text}
+# For each question, create a new entry in the database that looks like this:
+# oid_dict = {OID, option object}
+# counts = {quid, {oid 1:# of respondants, oid 2:# of respondants, oid 3:# of respondants}, ...}
+
+quid_dict = idDict('Question')
+oid_dict = idDict('Option')
+counts_dict = idDict('idDict')
+freq_dict = idDict('list')
+plot_dict = idDict('AxesSubplot')
+stop_dict = idDict('function')
+participant_dict = idDict('list')
+
+displayp = False
+
+
+def get_absolute_index_value(q, opts):
+
+    absolute_ordering = sorted(quid_dict[q.quid].options, key = lambda opt : opt.oid)
+
+    if q.qtype==qtypes["radio"] or q.qtype==qtypes["dropdown"] :
+        for (i, oid) in enumerate([opt.oid for opt in absolute_ordering]) :
+            if oid==opts[0].oid:
+                return i
+        assert 1==2, "oid %s not found" % opts[0].oid
+    elif q.qtype==qtypes["check"] : 
+        index = int("".join([{True : '1', False : '0'}[opt in opts] for opt in absolute_ordering]),2)
+        assert index > 0 and index <= pow(2, len(q.options)), "option ids %s not found" % [o.oid for o in opts]
+        return index
+    else : 
+        raise Exception("Question type not found")
+
+
+def add_to_participant_dict(survey_response):
+
+    #absolute_ordering = sorted(survey_response, key = lambda (q, _) : q.quid)    
+    #pid = uuid1()
+    #participant_dict[pid] = [get_absolute_index_value(q, opts) for (q, opts) in absolute_ordering]
+    participant_dict[uuid1()] = survey_response.sorted()
+
+
+def is_lazy(responses):
+    # this is a stupid way of doing things; should look at this more
+    # would like to do something involving entropy that's sensitive to location
+    # an even simpler start would be to throw out responses that have a run
+    # whose likelihood lies outside a 95% confidence interval
+    return all([oindices==responses[0] for oindices in [o for (q, o) in responses]])
+
+
+def is_outlier(responses):
+    # via hamming distance
+    # single object is a collection of bitstrings
+    # for a single question, we have a collection of options. differences will necessarily be 0 or 1 due to disjointness
+    # get total responses as a bitstrings of agreement
+    # KEEP BITSTRINGS FOR COMPARISON - should  be n^2 bitstrings total    
+    population_bitstrings = []
+    for ans1 in participant_dict.values():
+        qbitstrings = []
+        for ans2 in participant_dict.values():
+            # each set of responses is represented by a single bitstring.             
+            qbitstrings.append([{True : 0, False : 1}[i==j] for (i, j) in zip(ans1, ans2)])
+        population_bitstrings.append(qbitstrings)
+    population_hammings = [sum([sum(ans) for ans in part]) for part in population_bitstrings]
+    # compare the input responses with all other responses to generate new set of bitstrings for this response's hamming dist
+    absolute_ordering = [get_absolute_index_value(q, opts) for (q, opts) in sorted(responses, key = lambda (q, _) : q.quid)]
+    these_hammings = sum([sum([{True : 0, False : 1}[i==j] for (i, j) in zip(absolute_ordering, ans)]) for ans in participant_dict.values()])
+    return bootstrap(population_hammings)(these_hammings)
+
+
+def is_bot(responses):
+    # we will eventually mark questions that need to be consistent.
+    # consistency logic should be baked in to the app
+    return False
+
+
+def ignore(responses):
+    lazyp, outlierp, botp = is_lazy(responses), is_outlier(responses), is_bot(responses)
+    # need to record whether or not a response should be discarded and discard that response
+    # for later inspection
+    if outlierp:
+        with open((outdir or "") + 'outliers.txt', 'a') as f:
+            f.write((outformat or str)(responses))
+    return outlierp
+
+
+def classify_adversaries():
+    pass
+
+
+def bootstrap(samples, statistic = lambda x : sum(x) / (1.0 * len(x)), B  = 100):
+
+    n = len(samples)
+    if n < 5: return lambda x : False
+    bootstrap_samples = [statistic([samples[i] for i in np.random.random_integers(0, n-1, size=n)]) for _ in range(B)]
+    bootstrap_mean = sum(bootstrap_samples) / (1.0 * len(bootstrap_samples))
+    bootstrap_sd = pow(sum([pow(bsstat - bootstrap_mean, 2.0) for bsstat in bootstrap_samples]) / (B - 1), 0.5)
+
+    def retfun(test_value):
+        eps = abs(test_value - bootstrap_mean)
+        ninetyfive = 2 * bootstrap_sd
+        return [eps > ninetyfive, eps, ninetyfive]
+
+    return retfun
+    #return [s for s in samples if abs(s - bootstrap_mean) > 2*bootstrap_sd]
+
+
+def display(q, opts):
+
+    import matplotlib.pyplot as plt
+    import pylab
+    
+    def display_updated_image(quid):
+        fig = pylab.figure(1)
+        sub = plot_dict[quid]
+        sub.cla()
+        ct = sum(freq_dict[quid])
+        pdf = [f / ct for f in freq_dict[quid]]
+        for (x,y) in enumerate(pdf):
+            sub.vlines(x, 0, y)
+            pylab.draw()
+        sub.set_xlabel('%s' % q.qtext)
+        sub.set_ylabel('%s' % ('percent'))
+        # the 'M' will be a 'D' if we ever have something continuous, e.g. scrolling bar thing
+        pylab.ion()
+        pylab.show()
+    
+    def update_pdf(q, opts):
+        n = sum(freq_dict[q.quid])
+        updateindex = get_absolute_index_value(q, opts)
+        freq_dict[q.quid]=[freq + {updateindex : 1.0}.get(oindex, 0.0) for (oindex, freq) in enumerate(freq_dict[q.quid])]
+        #return true if successful
+        return n+1==sum(freq_dict[q.quid])
+    
+    assert update_pdf(q, opts)
+    display_updated_image(q.quid)
+
+
+def parse(input_file_name):
+
+    f = open(input_file_name, 'r')
+    json_obj = json.loads(f.read())
+    f.close()
+    qlist = [Question("", [""], 0) for _ in json_obj]
+
+    for (i, q) in enumerate(json_obj):
+        for (k, v) in q.iteritems():
+            if k=='options':
+                option_list = [Option(m['otext']) for m in v]
+                for (index, option) in enumerate(option_list):
+                    if v[index].has_key('oid'):
+                        option.__dict__['oid'] = uuid.UUID(v[index]['oid'])
+                v = option_list
+            elif k=='quid':
+                v = uuid.UUID(v)
+            qlist[i].__dict__[k]=v    
+
+    return Survey(qlist)
+
+
+def launch(survey, stop_condition):
+
+    qs = survey.questions
+
+    def initial_freq_dict(q):
+        if q.qtype==qtypes["radio"] or q.qtype==qtypes["dropdown"]:
+            return [0 for _ in q.options]
+        elif q.qtype==qtypes["check"]:
+            return [0 for _ in range(pow(2, len(q.options)))]
+
+    ##### initialize dictionaries #####
+    for (i, q) in enumerate(qs, 1):
+        quid_dict[q.quid] = q
+        for o in q.options:
+            size = len(oid_dict)
+            oid_dict[o.oid] = o
+            if not counts_dict.has_key(q.quid):
+                counts_dict[q.quid] = idDict('int')
+            counts_dict[q.quid][o.oid] = 0
+        freq_dict[q.quid] = initial_freq_dict(q)
+        if displayp:
+            from  matplotlib.pyplot import subplot,figure
+            fig = figure(1, figsize=(10,10))
+            fig.subplots_adjust(hspace=2.0, wspace=2.0)
+            sqrt = int(pow(len(qs), 0.5))
+            sub = fig.add_subplot(int(str(sqrt*sqrt+1)+str(sqrt)+str(i)))
+            plot_dict[q.quid] = sub
+
+    ##### where the work is done #####
+    sys.stdout.write("Querying participants")
+    while not stop_condition():
+        survey.shuffle()
+        # get one taker's responses
+        survey_response = SurveyResponse(get_response(survey))
+        add_to_participant_dict(survey_response)
+        sys.stdout.write(".")
+        sys.stdout.flush()
+        if not ignore(survey_response):
+            for (question, option_list) in survey_response:
+                for option in option_list:
+                    counts_dict[question.quid][option.oid] += 1
+                if displayp:
+                    display(question, option_list)
+
+    return { "quid_dict" : quid_dict
+             ,"oid_dict" : oid_dict
+             ,"counts_dict" : counts_dict
+             ,"freq_dict" : freq_dict
+             ,"plot_dict" : plot_dict 
+             ,"participant_dict" : participant_dict }
+
+
+if __name__=="__main__":
+    f, survey, stop, outdir, outformat, get_response = [None]*6
+    argmap = { "display" : lambda x : "displayp = " + x
+               ,"simulation" : lambda x : "execfile('" + x + "')\n"
+               ,"file" : lambda x : "f = " + x
+               ,"stop" : lambda x : "stop = " + x 
+               ,"outdir" : lambda x : "outdir = '" + x + {True : "", False : os.sep}[x.endswith(os.sep)] + "'"
+               ,"outformat" : lambda x : "outformat = " + x 
+               , "responsefn" : lambda x : "get_response = " + x}
+    if len(sys.argv)==2 and sys.argv[1]=="help":
+        print __doc__
+        exit(1)
+    for arg in sys.argv[1:]:
+        k, v = arg.split("=")
+        exec(argmap[k](v), globals())
+    assert survey or f, "One of simulation or file args must be set. For more information on how to use launcher, provide the argument 'help'"
+    if f:
+        def get_response(survey):
+            raise "Live version not yet implemented"
+    for (fname, d) in launch(survey or parse(f), stop or no_outliers).iteritems():
+        name, ext = fname.split("_")
+        with open((outdir or "") + name + "." + ext, 'w') as f:
+            f.write((outformat or str)(d))
---
+++ src/python/survey/objects.py
@@ -0,0 +1,157 @@
+# basic idea of the questionnaire is to provide the first pass of representing 
+# the survey
+
+# todo:
+# - Survey needs a function to proffer the survey and get a response. This response
+#   should be returned as a some kind of collection of data containing the actual responses,
+#   the question ids (since they might be shuffled) and maybe a confidence or something
+# - typed list?
+
+from uuid import uuid1
+from UserDict import UserDict
+import random
+import sys
+
+qtypes = {"freetext" : 0 , "radio" : 1 , "check" : 2 , "dropdown" : 3}
+
+class idDict(UserDict):
+
+    def __init__(self, valtype):
+        self.str_valtype=valtype
+        self.data={}
+        self.__add = self.__add_fn(valtype)
+
+    def __add_fn(self,str_valtype):
+        self.str_valtype
+        def __add_aux(uid, val):
+            assert uid.__class__.__name__=='UUID', "uid is of type %s; should be UUID" % type(uid).__name__
+            assert val.__class__.__name__==str_valtype, "val is of type %s; should be %s" % (type(val).__name__, str_valtype)
+            self.data[uid]=val
+        return __add_aux
+
+    def __setitem__(self, k, v):
+        self.__add(k, v)
+
+    def __getitem__(self, k):
+        return self.data[k]
+
+    def __getstate__(self):
+        return self.data
+
+class SurveyResponse:
+    
+    def __init__(self, *args):
+        self.response=[]
+        if len(args)==1:
+            self.response = args[0]
+        elif len(args)==2:
+            for tupe in zip(args[0], args[1]):
+                #tupe is (question, option_list)
+                self.response.append(tupe)
+        else:
+            raise Exception("Too many arguments (must be 1 or 2)")
+
+    def __iter__(self):
+        for r in self.response: yield r
+
+    def __getitem__(self, i):
+        return self.response[i]
+
+    def __len__(self):
+        return len(self.response)
+        
+    def jsonize(self):
+        return [{ "question" : question.jsonize(), "option_list" : [opt.jsonize() for opt in option_list] } \
+                for (question, option_list) in self.response]
+
+    def sorted(self):
+        return sorted([(question, sorted(opt_list, key = lambda opt : opt.oid)) for (question, opt_list) in self.response], key = lambda (q, _) : q.quid)
+
+class Survey :
+    
+    def __init__(self, questions):
+        self.questions = questions
+
+    def jsonize(self):
+        return [q.jsonize() for q in self.questions]
+        
+    def shuffle(self):
+        random.shuffle(self.questions)
+        for question in self.questions:
+            if (question.ok2shuffle):
+                random.shuffle(question.options)
+                question.reset_oindices()
+                
+    def show_question(self, q):
+        print q
+        return
+                
+    def read_response(self):
+        s = sys.stdin.readline()
+        return s
+            
+    def take_survey(self):
+        responses = []
+        for question in self.questions:
+            self.show_question(question)
+            responses.append(self.read_response().strip(), question.quid)
+        return responses      
+            
+
+class Option :
+    
+    def __init__(self, otext, inid = None):
+        self.otext = str(otext)
+        self.oid = uuid1()
+        self.inid = inid
+        self.oindex = None
+        
+    def jsonize(self):
+        return {"oid" : self.oid.hex, "otext" : self.otext}
+
+    def __repr__(self):
+        return self.otext
+        
+    def __str__(self):
+        return self.otext
+        
+
+class Question : 
+
+    def __init__(self, qtext, options, qtype, shuffle=False):
+        assert(qtype >= 0 and qtype < len(qtypes))
+        self.quid = uuid1()
+        self.qtext = qtext
+        self.options = []
+        optloc = 0
+        for option in options:
+            opt = Option(option)
+            opt.oindex = optloc
+            optloc += 1
+            self.options.append(opt)
+        assert(all([isinstance(o, Option) for o in self.options]))
+        self.ok2shuffle = shuffle
+        self.qtype=qtype
+
+    def jsonize(self):
+        return {"quid" : self.quid.hex
+                , "qtext" : self.qtext
+                , "ok2shuffle" : self.ok2shuffle
+                , "qtype" : self.qtype
+                , "options" : [o.jsonize() for o in sorted(self.options, key = lambda opt : opt.oindex)]}
+
+    def reset_oindices(self):
+        for (oindex, option) in enumerate(self.options):
+            option.oindex=oindex
+        
+    def __repr__(self):
+        val = self.qtext+"\n"
+        # for i in range(len(self.options)):
+        #     val = val + "\t" + str(i) + ". " + str(self.options[i]) + "\n"
+        return val
+        
+    def __str__(self):
+        val = self.qtext+"\n"
+        for i in range(len(self.options)):
+            val = val + "\t" + str(i) + ". " + str(self.options[i]) + "\n"
+        return val
