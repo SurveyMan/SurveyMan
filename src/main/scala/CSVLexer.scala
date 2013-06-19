382e1386c1a1317c060dcb1e2bff09c687fc87a0
etosch <etosch@clunky.cs.local>
2013-06-18 22:54:53 -0400

    building with maven. java fails; need to merge new stuff

---
+++ src/main/scala/CSVLexer.scala
@@ -0,0 +1,37 @@
+import scala.io.BufferedSource
+import java.io.FileInputStream
+import java.io.EOFException
+
+object CSVParser {
+
+  var counter = 0
+  def inc() = { counter+=1 ; counter }
+
+  def readLine(bs : BufferedSource) : String =  {
+    try {
+      new String(bs.takeWhile((c : Char) => c != '\n').toArray)
+    } catch {
+      case eof : EOFException => ""
+    }
+  }
+
+  def getHeaders(line : String) : Array[Symbol] = {
+    // if there's a header, set it to lower and make it a symbol
+    // if there's no header, generate a symbol
+    line.split(",").map((s : String) => Symbol(if (s == "") "gen"+inc() ; s.toLowerCase))
+  }
+  
+  def main(args : Array[String]) {
+    val encoding = ""
+    val bs = new BufferedSource(new FileInputStream(args(0)))
+    var line = readLine(bs)
+    // process first line
+    val headers : Array[Symbol] = getHeaders(line)
+    do {
+      line = readLine(bs)
+      // split each remaining line as data of the associated type
+      print(line)
+    } while ( line != '\n')
+  }
+
+}
