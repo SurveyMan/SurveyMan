59a86883eaaa20052829071849bb5e7874be6699
Emma Tosch <etosch@gmail.com>
2013-06-26 09:33:38 -0400

    now have java lexer using scala objects

--- src/main/java/csv/CSVLexer.java
+++ src/main/java/csv/CSVLexer.java
@@ -1,35 +1,36 @@
 package csv;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.FileReader;
-import java.lang.String;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import csv.CSVEntry;
-import QuotMarks;
+import scala.collection.Seq;
+import scalautils.QuotMarks;
 
 public class CSVLexer {
 
-    public final String seperator;
+    public final String seperator = ",";
 
     public static final String[] knownHeaders =
             {"QUESTION", "BLOCK", "OPTIONS", "RESOURCE", "EXCLUSIVE", "ORDERED", "PERTURB", "BRANCH"};
 
-    private boolean inQuot(String line) {
+    private static boolean inQuot(String line) {
         // searches for quotation marks
         // since there are multiple possibilities for the right qmark,
         // consider the first match the matching one
         // only care about the outer quotation.
-        String[] c = line.toCharArray();
+        char[] c = line.toCharArray();
         boolean inQ = false;
         String lqmark = null;
-        List<String> rqmarks = null;
+        Seq<String> rqmarks = null;
         int i = 0;
         while (i < c.length) {
-            String s = new String(c[i]);
+            String s = String.valueOf(c[i]);
             if (QuotMarks.isA(s)) {
                 if (inQ) {
+                    assert (rqmarks!=null);
                     if (rqmarks.contains(s)) {
                         if (i+1 < c.length && c[i]==c[i+1]) // valid escape seq
                             i++;
@@ -37,8 +38,8 @@ public class CSVLexer {
                     }
                 } else {
                     // if I'm not already in a quote, check whether this is a 2-char quot.
-                    if (i+1 < c.length && QuotMars.isA(s + new String(c[i+1]))) {
-                        lqmark = sprime; i++;
+                    if (i+1 < c.length && QuotMarks.isA(s + String.valueOf(c[i+1]))) {
+                        lqmark = s + String.valueOf(c[i+1]); i++;
                     } else lqmark = s ;
                     inQ=true ; rqmarks = QuotMarks.getMatch(lqmark);
                 }
@@ -48,7 +49,8 @@ public class CSVLexer {
         return inQ;
     }
 
-    public static ArrayList<List<CSVEntry>> lex(String filename) {
+    public static ArrayList<ArrayList<CSVEntry>> lex(String filename) 
+            throws FileNotFoundException, IOException {
         // FileReader uses the system's default encoding.
         // BufferedReader makes 16-bit chars
         BufferedReader br = new BufferedReader(new FileReader(filename));
@@ -56,14 +58,15 @@ public class CSVLexer {
         String line = "";
         while((line = br.readLine()) != null) {
             // check to make sure this isn't a false alarm where we're in a quot
-            while (inQuot(line))
+            while (CSVLexer.inQuot(line))
                 line  = line + br.readLine();
         }
-        return new ArrayList<List<CSVEntry>>();
+        return new ArrayList<ArrayList<CSVEntry>>();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+            throws FileNotFoundException, IOException {
         //write test code here
-        System.out.println("Hello, World!");
-    }
+        ArrayList<ArrayList<CSVEntry>> retval = lex(args[0]);
+   }
 }
