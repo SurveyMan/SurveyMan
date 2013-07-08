2fbaf8048cca4ec79af3c9cc88b4be3cd6108b4c
Molly K. McMahon <wester3450@aim.com>
2013-07-08 15:25:42 -0400

    going to merge

--- src/main/java/qc/Stat.java
+++ src/main/java/qc/Stat.java
@@ -4,27 +4,29 @@
  */
 package qc;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Molly
  */
 public class Stat {
     
-    public static double mean(double[] values){
+    public static double mean(ArrayList<Double> values){
         double sum = 0;
         for(double d : values){
             sum+=d;
         }
-        return sum/(values.length);
+        return sum/(values.size());
     }
     
-    public static double stddev(double[] values){
+    public static double stddev(ArrayList<Double> values){
         double mean = mean(values);
         double sum = 0;
         for(double d: values){
             sum+=Math.pow(Math.abs(mean-d),2);
         }
-        return Math.sqrt(sum/(values.length));
+        return Math.sqrt(sum/(values.size()));
         
     }
     
