b7206494325e95816acbfb8781925e82e591da72
Jessica Newman <jnewman@cs.umass.edu>
2013-07-08 20:42:54 -0400

    Changed check for insufficient funds to check against the reward specified in the parameters file.

--- src/main/java/system/mturk/SurveyPoster.java
+++ src/main/java/system/mturk/SurveyPoster.java
@@ -35,7 +35,7 @@ public class SurveyPoster {
     public static boolean hasEnoughFund() {
         double balance = service.getAccountBalance();
         System.out.println("Got account balance: " + RequesterService.formatCurrency(balance));
-        return balance > 0;
+        return balance > parameters.getRewardAmount();   // should check for whether balance >
     }
     
     public static void expireOldHITs() {
