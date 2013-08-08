32a0202039cfe74da255145ac6d17fda27e7745c
etosch <etosch@gmail.com>
2013-08-08 19:23:18 -0400

    making setup easier (I hope)

--- src/main/java/SurveyMan.java
+++ src/main/java/SurveyMan.java
@@ -5,80 +5,111 @@ import javax.swing.text.JTextComponent;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * I would like to acknowledge StackOverflow and the liberal copying I employed to make this Swing crap work.
+ */
 
 public class SurveyMan extends JPanel implements ActionListener{
 
     public JFrame frame = new JFrame("SurveyMan");
-    public JPanel content = new JPanel(new GridLayout(3,1));
+    public JPanel content = new JPanel(new BorderLayout());
     public JButton next1 = new JButton("Next");
+    public JButton next2 = new JButton("Next");
 
     public void actionPerformed(ActionEvent actionEvent) {
         if (actionEvent.getActionCommand().equals(next1.getActionCommand())) {
-            content = new JPanel(new GridLayout(3,1));
-            frame.setContentPane(frame2(content));
+            content = new JPanel(new BorderLayout());
+            frame.setContentPane(setup_frame2(content));
+            frame.setVisible(true);
+            frame.getContentPane().setPreferredSize(new Dimension(800, 600));
+            frame.pack();
+        } else if (actionEvent.getActionCommand().equals(next2.getActionCommand())) {
+            content = new JPanel(new BorderLayout());
+            frame.setContentPane(setup_frame3(content));
         }
     }
 
-    private void frame1(JPanel content) {
+    private void setup_frame1(JPanel content) {
 
         JLabel note = new JLabel("<html>" +
-                "It appears you haven't yet set your SURVEYMAN configuration directory." +
-                "<br>Please choose a location for the configuration directory and create an environment variable named SURVEYMAN." +
-                "<br>A suggestion is to create a folder called ~/.surveyman." +
-                "<ul>" +
-                "<li>Linux: add 'export SURVEYMAN=$HOME/.surveyman' to .bashrc in your home directory.</li>" +
-                "<li>OSX: add 'export SURVEYMAN=$HOME/.surveyman' to .bash_profile in your home directory.</li>" +
-                "<li>Windows: create a system environment variable called SURVEYMAN and set it equal to %HOME%\\.surveyman</li>" +
-                "</ul>" +
+                "It appears you haven't yet configured your Surveyman directory." +
+                "<br>Your metadata, access keys, and output will appear in this folder." +
+                "<br>" +
+                "<br>Your Surveyman home directory is located at :" +
+                "<br>"+Library.DIR +
+                "<br>Now you will need to download your keys to this directory.<br>" +
+                "<br><b>WARNING:</b> You will need both AWS and Amazon Mechanical Turk accounts to continue." +
+                "<br>Register at https://aws.amazon.com and https://www.mturk.com." +
+                "<br>Click the Next button to continue." +
                 "</html>");
         note.setVerticalAlignment(JLabel.CENTER);
         note.setHorizontalAlignment(JLabel.CENTER);
-        content.add(note);
+        content.add(note, BorderLayout.CENTER);
 
-//        final JButton button = new JButton("Choose config directory");
-//        button.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent actionEvent) {
-//                final JFileChooser fc = new JFileChooser();
-//                fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-//                int retval = fc.showOpenDialog(button);
-//                fc.addActionListener(new ActionListener() {
-//                    @Override
-//                    public void actionPerformed(ActionEvent actionEvent) {
-//                        while (! actionEvent.getActionCommand().equals(JFileChooser.APPROVE_SELECTION)) {}
-//                        //Library.DIR = fc.getSelectedFile().getAbsolutePath();
-//                        try {
-//                            Runtime.getRuntime().exec(String.format("export SURVEYMAN=%s", fc.getSelectedFile().getAbsolutePath()));
-//                        } catch (IOException e) {
-//                            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-//                        }
-//                    }
-//                });
-//            }
-//        });
-        //content.add(button);
         next1.addActionListener(this);
-        while(System.getenv().get("SURVEYMAN")==null) {
-            System.out.println(System.getenv().keySet());
-        }
-        content.add(next1);
+        content.add(next1, BorderLayout.SOUTH);
         frame.setContentPane(content);
     }
 
-    private JPanel frame2(JPanel content) {
+    private JPanel setup_frame2(JPanel content) {
+
+        JPanel stuff = new JPanel(new BorderLayout());
+
+        JLabel note = new JLabel("<html>" +
+                "Now you will need to add your keys to the Surveyman folder. " +
+                "<br>Navigate to:" +
+                "<br><br>https://console.aws.amazon.com/iam/home?#security_credential" +
+                "<br>and create new keys. If available, download them. If the download option is not available," +
+                "<br>save them in a file with the following format:" +
+                "<p>access_key=&lt;your_access_key&gt;</p>" +
+                "<p>secret_key=&lt;your_secret_key&gt;</p>" +
+                "<br>When you're ready, select the location of this file." +
+                "</html>");
+        stuff.add(note, BorderLayout.CENTER);
+
+        final JButton button = new JButton("Choose the keys file.");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                final JFileChooser fc = new JFileChooser();
+                fc.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        if (actionEvent.getActionCommand().equals(JFileChooser.APPROVE_SELECTION)) {
+                            File keyFile = new File(fc.getSelectedFile().getAbsolutePath());
+                            keyFile.renameTo(new File(Library.CONFIG));
+                        }
+                    }
+                });
+                fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                fc.showOpenDialog(button);
+            }
+        });
+        stuff.add(button, BorderLayout.SOUTH);
+        stuff.add(new JPanel(), BorderLayout.EAST);
+
+        try {
+            Desktop.getDesktop().browse(new URI("https://console.aws.amazon.com/iam/home?#security_credential"));
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (URISyntaxException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        content.add(stuff,BorderLayout.CENTER);
+
+        next2.addActionListener(this);
+        content.add(next2, BorderLayout.SOUTH);
         return content;
     }
 
-    public void run() {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setMenuBar(new MenuBar());
-        frame.setContentPane(content);
-        frame.pack();
-        frame.setVisible(true);
-        if (System.getenv().get("SURVEYMAN")==null) {
-            frame1(content);
-        }
+    private JPanel setup_frame3(JPanel content) {
+        return content;
     }
 
     public static boolean surveyman_env_var(){
@@ -87,9 +118,30 @@ public class SurveyMan extends JPanel implements ActionListener{
         return false;
     }
 
+    public void setup(){
+        // set up UI
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMenuBar(new MenuBar());
+        frame.setContentPane(content);
+        frame.getContentPane().setPreferredSize(new Dimension(800, 600));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        setup_frame1(content);
+        // try creating new directory
+        (new File(Library.DIR)).mkdir();
+        // load web page
+    }
+
 
     public static void main(String[] args) {
-        surveyman_env_var();
-        //(new SurveyMan()).run();
+        SurveyMan sm = new SurveyMan();
+        sm.next1.setPreferredSize(new Dimension(100, 30));
+        if ((new File(Library.DIR)).isDirectory()) {
+
+        } else {
+            // create surveyman folder
+            sm.setup();
+        }
     }
 }
