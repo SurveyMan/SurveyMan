6a2e0bfa802fd828940e2b80452aebc18e76987c
etosch <etosch@gmail.com>
2013-08-23 14:43:47 -0400

    #114

--- src/main/java/gui/SurveyMan.java
+++ src/main/java/gui/SurveyMan.java
@@ -6,9 +6,11 @@ import gui.display.Setup;
 import org.apache.log4j.*;
 import survey.Survey;
 import system.Library;
+import system.mturk.MturkLibrary;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
 
 /**
  * I would like to acknowledge StackOverflow and the liberal copying I employed to make this Swing crap work.
@@ -32,10 +34,26 @@ public class SurveyMan {
             System.exit(-1);
         }
     }
+
+    public static void flushOldLogs() {
+        long MILLIS_SEC = 1000
+                , SEC_MIN = 60
+                , MIN_HR = 60
+                , HR_DAY = 24
+                , DAY_MON = 30;
+        long today = System.currentTimeMillis();
+        long thirtyDays = MILLIS_SEC * SEC_MIN * MIN_HR * HR_DAY * DAY_MON;
+        long monthAgo = today - thirtyDays;
+        for (File f : (new File("logs")).listFiles())
+            if (f.lastModified() < monthAgo)
+                f.delete();
+    }
+
     public static void main(String[] args) {
         if (!(new File(Library.DIR)).isDirectory()) {
             Setup.run();
         } else {
+            flushOldLogs();
             Experiment.run();
         }
     }
