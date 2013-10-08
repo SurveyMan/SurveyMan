5b9c1b86ddcd703aafdf7c2dfebf3a00159c0c96
etosch <etosch@gmail.com>
2013-10-07 20:34:29 -0400

    more from the challenge dataset

--- src/python/challenge.py
+++ src/python/challenge.py
@@ -2,6 +2,8 @@ import csv
 import matplotlib
 import matplotlib.pyplot as pyplot
 import datetime
+import math
+
 
 reader = csv.reader(open("/Users/etosch/dev/SurveyMan-public/data/crowdflower_sentiment.csv", "rU"))
 header = True
@@ -135,13 +137,65 @@ pyplot.xlabel('Number of pages submitted')
 pyplot.ylabel('Number of respondents submitting after viewing this many pages')
 pyplot.savefig('pagesviewed')
 
-# check both ways of computing likelihood
+# naive likelihood - don't take into account position at all
+# also doesn't care about the number of responses made
+def naive_likelihood(response_list):
+    prob = 0.0
+    for response in response_list:
+        quid = response['questionid']
+        ans = response['answer']
+        prob = prob + math.log(question_answer_freq[quid][ans]/(sum(question_answer_freq[quid].values())*1.0))
+    return prob*-1.0
+    
 
-for respondent_id in responses.keys():
-    ans = 0
-    for response in responses[respondent_id]:
-        this_qs_fmap = question_answer_freq[response['questionid']]
-        this_ans_prob = 
+likelihoods = [(response_id, naive_likelihood(responses[response_id])) for response_id in responses.keys()]
+
+# look for outliers in the likelihoods    
+# assume normal distribution? plot likelihoods
+pyplot.hist([pair[1] for pair in likelihoods], bins=100)
+pyplot.savefig("likelihoods")
+mean = sum([pair[1] for pair in likelihoods])/(len(likelihoods)*1.0)
+sd = pow(sum([pow(pair[1]-mean, 2) for pair in likelihoods])/len(likelihoods), 0.5)
+# print out response ids having outlier likelihoods
+potential_outliers = []
+for (response_id, likelihood) in likelihoods:
+    if abs(likelihood-mean) > 2*sd:
+        potential_outliers.append((response_id, likelihood))
+        print response_id
+
+# load in gold standard answers 
+gold_standard = []
+header = True
+for row in csv.reader(open("/Users/etosch/dev/SurveyMan-public/data/reference_30_.csv", "rU")):
+    if header:
+        header = False
+    else:
+        gold_standard.append((row[0], row[1]))
+
+# find out how our outliers did on the gold standard data
+def assess(population):
+    evaluation_outliers = []
+    for respondent_id in population:
+        num_correct = 0
+        num_incorrect = 0
+        num_unanswered = 0
+        for (questionid, answer) in gold_standard:
+            response = [r for r in responses[respondent_id] if r['questionid']==questionid]
+            if len(response)==0:
+                num_unanswered+=1
+            elif response[0]['answer']==answer:
+                num_correct+=1
+            else:
+                num_incorrect+=1
+        print respondent_id, num_correct, num_incorrect, num_unanswered
+        evaluation_outliers.append((respondent_id, num_correct, num_incorrect, num_unanswered))
+    return evaluation_outliers
+
+evaluation_outliers = assess([p[0] for p in potential_outliers])
+evaluation_good_guys = assess([r for r in responses.keys() if r not in [p[0] for p in potential_outliers]])
+
+# DEFINITELY NEED TO CONTROL FOR LENGTH
+
 
 
 # want a visual representation of overlap
