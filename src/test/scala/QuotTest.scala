2ce4a18afa8a69ce1b055b150102842bbac5150b
etosch <etosch@gmail.com>
2013-10-28 11:33:11 -0400

    oops, forgot quottest

--- src/test/scala/QuotTest.scala
+++ src/test/scala/QuotTest.scala
@@ -1,24 +1,11 @@
-import csv.CSVLexer
-import java.io.IOException
-import org.apache.log4j.{SimpleLayout, Level, FileAppender, Logger}
+import org.apache.log4j.{Logger}
 import org.junit._
 import scalautils.QuotMarks
 import scalautils.QuotMarks.UnicodeQuot
 
 class QuotTest {
-  private val LOGGER : Logger = Logger.getRootLogger()
-  LOGGER.setLevel(Level.ALL)
-  private var txtHandler : FileAppender = null
-  try {
-    txtHandler = new FileAppender(new SimpleLayout(), "logs/CSVTest.log")
-    txtHandler.setEncoding("UTF-8")
-    LOGGER.addAppender(txtHandler)
-  } catch {
-    case io : IOException => {
-      println (io.getMessage())
-      System.exit(-1)
-    }
-  }
+  private val LOGGER : Logger = Logger.getLogger(classOf[QuotTest])
+
   @Test def testQuots {
     for ((UnicodeQuot(l, r), _) <- QuotMarks.quotpairs)
       LOGGER.info(f"left: $l%s, right: $r%s")
