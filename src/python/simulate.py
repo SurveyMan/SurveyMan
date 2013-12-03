e789a47ec50fa7ee945c74601bf8f4da35663109
emeryberger <emery.berger@gmail.com>
2013-12-03 16:07:04 -0500

    Cleanup.

--- src/python/simulate.py
+++ src/python/simulate.py
@@ -6,7 +6,7 @@
 #
 
 from random import randint
-from math   import factorial, log
+from math   import log
 
 def entropy(l):
     "Compute the entropy of a list of values."
@@ -20,23 +20,13 @@ def entropy(l):
     else:
         return -ent
 
-def simulate(N = 50,            # number of coins (= # of questions)
-             coinSides = 4,     # number of sides to each coin (= # of answers per question)
-             percentBias = 40,  # % bias over expected
-             conf = 0.95):      # desired confidence level
 
-    p = 1.0 / coinSides          # probability of heads if unbiased
-    bias = (p * (1.0 + percentBias / 100.0))  # % chance of heads
-    biasNum = int(round(bias * N))
-
-    # Let's compute the likelihood empirically (via Monte Carlo
-    # simulation) that N coin flips yields a biased number of heads (given
-    # as "bias" % above).
-
-    simulations = 10000 # number of simulations to run
-    totalBiased = 0    # number of simulations that appear biased at conf level
-   
-    biased = 0
+def simulate (N = 50,
+              coinSides = 4,
+              confidence = 0.95,
+              simulations = 10000):
+    "Simulate coin flips to establish confidence level on entropy."
+    simulations = 10000 
     # Pre-compute normalized max entropy.
     maxEntFlips = [(N * 1.0/coinSides) for x in range(0, coinSides)]
     maxEnt = entropy(maxEntFlips)
@@ -54,19 +44,16 @@ def simulate(N = 50,            # number of coins (= # of questions)
             flips[v] += 1
         # Add the normalized entropy.
         entropies.append ((flips, entropy(flips) / maxEnt))
-        # Find the maximum entry.
-        maximum = reduce(max, flips)
-        # If the max was the biased amount or larger, count the throw as biased.
-        if (maximum >= int(round(bias * N))):
-            biased += 1
 
     entropies.sort()
-    print "%f%% of entropies higher than %f" % (100 * conf, entropies[int((1.0 - conf) * simulations)][1])
-    print entropies[int((1.0 - conf) * simulations)][0]
-    print "biased = %d, fraction = %f"  % (biased, biased * 1.0 / simulations)
-    print "N = %d, expected = %f, biased = %f" % (N, p * N, int(round(bias * N)))
-    print "unbiased fraction = %f%%" % (1.0 - (biased / (1.0 * simulations)))
+    print "%f%% of entropies greater than %f" % \
+    (100 * confidence, entropies[int((1.0 - confidence) * simulations)][1])
 
-simulate(N = 50, coinSides = 2)
+    print entropies[int((1.0 - confidence) * simulations)][0]
+#    print "biased = %d, fraction = %f"  % (biased, biased * 1.0 / simulations)
+#    print "N = %d, expected = %f, biased = %f" % (N, p * N, int(round(bias * N)))
+#    print "unbiased fraction = %f%%" % (1.0 - (biased / (1.0 * simulations)))
+
+simulate(N = 50, coinSides = 8, confidence = 0.90)
 
 
