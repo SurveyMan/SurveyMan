74cddc9d1aefdb1265d45a91cf9e16bece98a61d
Emma Tosch <etosch@gmail.com>
2014-01-17 08:14:06 -0500

    blocking done ; now we need to connect up with definitons.js

--- src/javascript/randomize.js
+++ src/javascript/randomize.js
@@ -4,194 +4,193 @@
 
 var SurveyMan = function (jsonSurvey) {
 
-    this.allQuestions = [];
+    var allQuestions        =   [],
+        getQuestionById     =   function (quid) {
 
-    var range = function (n) {
-        var i, rList = [];
-        for ( i = 0 ; i < n ; i++ ) {
-            rList.push(i);
-        }
-        return rList;
-    }
+                                    var i;
+                                    for ( i = 0 ; i < this.allQuestions.length ; i++ ) {
+                                        if ( this.allQuestions[i].idString === quid ) {
+                                            return this.allQuestions[i];
+                                        }
+                                    }
+                                    throw "Question id " + quid + " not found in allQuestions";
 
-    var Block = function(jsonBlock) {
+                                },
+        range               =   function (n) {
 
-        var idStringToArray = function (_idString) {
-            return _.map(_idString.split("."), function(s) { parseInt(s); });
-        }
+                                    var i, rList = [];
+                                    for ( i = 0 ; i < n ; i++ ) {
+                                        rList.push(i);
+                                    }
+                                    return rList;
 
-        this.idString = jsonBlock.id;
-        this.idArray = idStringToArray(this.idString);
-        this.topLevelQuestions = Question.makeQuestions(jsonBlock.questions, this);
-        this.branchParadigm = jsonBlock.branchParadigm;
-        this.branchQuestion = this.getQuestion(jsonBlock.branchQuestionId);
-        this.subblocks = [];
-        this.randomizable = jsonBlock.randomize;
-        this.getQuestion = function(quid) {
-            var i;
-            for ( i = 0 ; i < this.topLevelQuestions.length ; i++ ) {
-                if ( this.topLevelQuestions[i].idString == quid ) {
-                    return this.topLevelQuestions[i];
-                }
-            }
-            throw "Question with id " + quid + " not found in block " + this.idString;
-        };
-        this.idComp = function(that) {
-            // returns whether that follows (+1), precedes (-1), or is a sub-block (0) of this
-            var i, j;
-            for ( i = 0 ; i < this.idArray ; i++ ) {
-                if ( i < that.idArray.length ) {
-                    if ( this.idArray[i] < that.idArray[i] ) {
-                        return -1;
-                    } else if ( this.idArray[i] > that.idArray[i] ) {
-                        return 1;
-                    }
-                }
-                return 0;
-            }
-        }
-        this.randomize = function () {
-            var i, j, newSBlocks = _.map(range(this.subblocks.length), -1);
-            // randomize questions
-            _.shuffle(this.topLevelQuestions);
-            // randomize blocks
-            var stationaryBlocks = _.filter(this.subblocks, function (b) { return b.randomizable; }),
-                nonStationaryBlocks = _.filter(this.subblocks, function (b) { return ! b.randomizable; }),
-                samp = _.sample(range(this.subblocks.length), nonStationaryBlocks.length);
-            _.shuffle(nonStationaryBlocks);
-            for ( i = 0 ; i < samp.length ; i++ ) {
-                // pick the locations for where to put the non-stationary blocks
-                newSBlocks[samp[i]] = nonStationaryBlock[i];
-            }
-            for ( i = 0, j = 0; i < newSBlocks.length ; i++ ) {
-                if ( newSBlocks[i] == -1 ) {
-                    newSBlocks[i] = stationaryBlocks[j];
-                    j++;
-                }
-            }
-            console.assert(j == stationaryBlocks.length - 1);
-            this.subblocks = newSBlocks;
-            for ( i = 0 ; i < this.subblocks.length ; i++) {
-                this.subblocks.randomize();
-            }
-        };
-        this.populate = function () {
-            var i;
-            for ( i = 0 ; i < jsonBlock.subblocks.length ; i++ ) {
-                var b = new Block(jsonBlock.subblocks[i]);
-                this.subblocks.push(b);
-                b.populate();
-            }
-        }
-        // assert that the sub-blocks have the appropriate ids
-        console.assert(_.every(subBlocks, function(b) { this.idComp(b) == 0 }));
-    }
+                                },
+        Block               =   function(jsonBlock) {
 
-    var Option = function(jsonOption, _question) {
+                                    var idStringToArray = function (_idString) {
+                                        return _.map(_idString.split("."), function(s) { parseInt(s); });
+                                    };
 
-        var makeOptions = function (jsonOptions, enclosingQuestion) {
-            var i, oList = [];
-            for ( i = 0 ; i < jsonOptions.length ; i++ ){
-                oList.push(new Option(jsonOptions[i], enclosingQuestion));
-            }
-            return oList;
-        }
+                                    this.idString = jsonBlock.id;
+                                    this.idArray = idStringToArray(this.idString);
+                                    this.topLevelQuestions = Question.makeQuestions(jsonBlock.questions, this);
+                                    this.branchParadigm = jsonBlock.branchParadigm;
+                                    this.branchQuestion = this.getQuestion(jsonBlock.branchQuestionId);
+                                    this.subblocks = [];
+                                    this.randomizable = jsonBlock.randomize;
+                                    this.getQuestion = function(quid) {
+                                        var i;
+                                        for ( i = 0 ; i < this.topLevelQuestions.length ; i++ ) {
+                                            if ( this.topLevelQuestions[i].idString == quid ) {
+                                                return this.topLevelQuestions[i];
+                                            }
+                                        }
+                                        throw "Question with id " + quid + " not found in block " + this.idString;
+                                    };
+                                    this.idComp = function(that) {
+                                        // returns whether that follows (+1), precedes (-1), or is a sub-block (0) of this
+                                        var i, j;
+                                        for ( i = 0 ; i < this.idArray ; i++ ) {
+                                            if ( i < that.idArray.length ) {
+                                                if ( this.idArray[i] < that.idArray[i] ) {
+                                                    return -1;
+                                                } else if ( this.idArray[i] > that.idArray[i] ) {
+                                                    return 1;
+                                                }
+                                            }
+                                            return 0;
+                                        }
+                                    };
+                                    this.randomize = function () {
+                                        var i, j, newSBlocks = _.map(range(this.subblocks.length), -1);
+                                        // randomize questions
+                                        _.shuffle(this.topLevelQuestions);
+                                        // randomize blocks
+                                        var stationaryBlocks = _.filter(this.subblocks, function (b) { return b.randomizable; }),
+                                            nonStationaryBlocks = _.filter(this.subblocks, function (b) { return ! b.randomizable; }),
+                                            samp = _.sample(range(this.subblocks.length), nonStationaryBlocks.length);
+                                        _.shuffle(nonStationaryBlocks);
+                                        for ( i = 0 ; i < samp.length ; i++ ) {
+                                            // pick the locations for where to put the non-stationary blocks
+                                            newSBlocks[samp[i]] = nonStationaryBlock[i];
+                                        }
+                                        for ( i = 0, j = 0; i < newSBlocks.length ; i++ ) {
+                                            if ( newSBlocks[i] == -1 ) {
+                                                newSBlocks[i] = stationaryBlocks[j];
+                                                j++;
+                                            }
+                                        }
+                                        console.assert(j == stationaryBlocks.length - 1);
+                                        this.subblocks = newSBlocks;
+                                        for ( i = 0 ; i < this.subblocks.length ; i++) {
+                                            this.subblocks.randomize();
+                                        }
+                                    };
+                                    this.populate = function () {
+                                        var i;
+                                        for ( i = 0 ; i < jsonBlock.subblocks.length ; i++ ) {
+                                            var b = new Block(jsonBlock.subblocks[i]);
+                                            this.subblocks.push(b);
+                                            b.populate();
+                                        }
+                                    };
+                                    // assert that the sub-blocks have the appropriate ids
+                                    console.assert(_.every(subBlocks, function(b) { this.idComp(b) == 0 }));
 
-        this.idString = jsonOption.id;
-        this.otext = jsonOption.otext;
-        this.question = _question;
+                                },
+        Option              =   function(jsonOption, _question) {
 
-    }
+                                    var makeOptions = function (jsonOptions, enclosingQuestion) {
+                                        var i, oList = [];
+                                        for ( i = 0 ; i < jsonOptions.length ; i++ ){
+                                            oList.push(new Option(jsonOptions[i], enclosingQuestion));
+                                        }
+                                        return oList;
+                                    }
 
-    var Question = function(jsonQuestion, _block) {
+                                    this.idString = jsonOption.id;
+                                    this.otext = jsonOption.otext;
+                                    this.question = _question;
 
-        var makeQuestions = function (jsonQuestions, enclosingBlock) {
-            var i, qList = [];
-            for ( i = 0 ; i < jsonQuestions.length ; i++ ) {
-                new Question(jsonQuestions[i], enclosingBlock);
-                qList.push(q);
-                allQuestions.push(q);
-            }
-            return qList;
-        };
+                                },
+        Question            =   function(jsonQuestion, _block) {
 
-        var makeBranchMap = function (branchMap, ) {
-            // branchMap -> map from oid to quid
-            if (!_.isUndefined(branchMap)) {
+                                    var makeQuestions   =   function (jsonQuestions, enclosingBlock) {
+                                                                var i, qList = [];
+                                                                for ( i = 0 ; i < jsonQuestions.length ; i++ ) {
+                                                                    var q = new Question(jsonQuestions[i], enclosingBlock);
+                                                                    qList.push(q);
+                                                                    allQuestions.push(q);
+                                                                }
+                                                                return qList;
+                                                            },
+                                        makeBranchMap   =   function (branchMap, _question) {
+                                                                var i, bm = {};
+                                                                // branchMap -> map from oid to quid
+                                                                if (!_.isUndefined(branchMap)) {
+                                                                    var keys = _.branchMap.keys();
+                                                                    for ( i = 0 ; i < keys.length ; i++ ) {
+                                                                        var o = _question.getOption(keys[i]),
+                                                                            q = getQuestionById(branchMap[keys[i]]);
+                                                                        bm[o] = q;
+                                                                    }
+                                                                }
+                                                                return bm;
+                                                            };
 
-            }
-        };
+                                    this.block = _block;
+                                    this.idString = jsonQuestion.id;
+                                    this.resource = jsonQuestion.resource;
+                                    this.options = Option.makeOptions(jsonQuestion.options);
+                                    this.branchMap = makeBranchMap(jsonQuestion.branchMap, this);
+                                    this.randomizable = jsonQuestion.randomize;
+                                    this.ordered = jsonQuestion.ordered;
+                                    this.exclusive = jsonQuestion.exclusive;
+                                    this.breakoff = jsonSurvey.breakoff;
+                                    this.getOption = function (oid) {
+                                        var i;
+                                        for ( i = 0 ; i < options.length ; i++ ) {
+                                            if ( options[i].idString === oid ) {
+                                                return options[i];
+                                            }
+                                        }
+                                        throw "Option id " + oid + " not found in question " + this.idString;
+                                    };
+                                    this.randomize = function () {
+                                        var i;
+                                        if (this.ordered) {
+                                            if (Math.random() < 0.5) {
+                                                options.reverse();
+                                            }
+                                        } else {
+                                            _.shuffle(options);
+                                        }
+                                    };
 
-        this.block = _block;
-        this.idString = jsonQuestion.id;
-        this.resource = jsonQuestion.resource;
-        this.options = Option.makeOptions(jsonQuestion.options);
-        this.branchMap = makeBranchMap(jsonQuestion.branchMap);
-        this.randomizable = jsonQuestion.randomize;
-        this.ordered = jsonQuestion.ordered;
-        this.exclusive = jsonQuestion.exclusive;
-        this.getOption = function (oid) {
-            var i;
-            for ( i = 0 ; i < options.length ; i++ ) {
-                if ( options[i].idString === oid ) {
-                    return options[i];
-                }
-            }
-            throw "Option id " + oid + " not found in question " + this.idString;
-        };
-        this.randomize = function () {
-            var i;
-            if (this.ordered) {
-                if (Math.random() < 0.5) {
-                    options.reverse();
-                }
-            } else {
-                _.shuffle(options);
-            }
-        };
-    };
+                                }
+        Survey              =   function (jsonSurvey) {
 
-    var Survey = function (jsonSurvey) {
+                                    var makeSurvey = function(jsonSurvey.survey) {
+                                        var i, blockList = [];
+                                        for ( i = 0 ; i < jsonSurvey.length ; i++ ) {
+                                            blockList[i] = new Block(jsonSurvey[i]).populate();
+                                        }
+                                    }
 
-        var makeSurvey = function(jsonSurvey.survey) {
-            var i, blockList = [];
-            for ( i = 0 ; i < jsonSurvey.length ; i++ ) {
-                blockList[i] = new Block(jsonSurvey[i]).populate();
-            }
-        }
+                                    this.filename = jsonSurvey.filename;
+                                    this.topLevelBlocks = makeSurvey(jsonSurvey);
+                                    this.randomize = function () {
+                                        var i;
+                                        for ( i = 0 ; i < this.topLevelBlocks.length ; i++ ) {
+                                            // contents of the survey
+                                            this.topLevelBlocks[i].randomize();
+                                        }
+                                    }
 
-        this.filename = jsonSurvey.filename;
-        this.breakoff = jsonSurvey.breakoff;
-        this.topLevelBlocks = makeSurvey(jsonSurvey);
-        this.randomize = function () {
-            var i;
-            for ( i = 0 ; i < this.topLevelBlocks.length ; i++ ) {
-                // contents of the survey
-                this.topLevelBlocks[i].randomize();
-            }
-        }
-
-    }
+                                };
 
     this.survey = Survey(jsonSurvey);
 
-var makeBlockList = function(JSONSurvey) {
-    randomizableBlocks = _.filter(JSONSurvey.keys(), function(m) { m['']})
-    return _.shuffle()
-}
-
-var setVars = function (JSONSurvey) {
-    // shuffle questions
-    blockList = makeBlockList(JSONSurvey)
-    questionList = []
-
-    // pick blocks
-    // every user has a unique view; keep views in a table, indexed by workerId hash
-    // user-unique data : firstQuestionId, qTransTable, branchTable, oneBranchTable, lastQuestionId, bList
-    // global data : qTable, oTable
-    // remove the data when the worker submits.
-     firstQuestionId =
-     qTransTable = makeTransTable()
 };
 
 /*
