f499309a5e17fb0ecd37791739c5a4af4828dbf2
Molly K. McMahon <wester3450@aim.com>
2014-02-18 19:38:47 -0500

    example representation of https://github.com/etosch/SurveyMan/blob/master/data/Ipierotis.csv (without blocking/country question)

--- src/python/survey/example_survey.py
+++ src/python/survey/example_survey.py
@@ -1,3 +1,4 @@
+# -*- coding: cp1252 -*-
 #create example survey
 #output JSON representation
 from survey_representation import *
@@ -73,14 +74,73 @@ def main():
     q9.addOption("2 or more races")
     q9.addOption("Unknown")
     #print q9
+    #question 10
+    q10 = Question("radio", "Why do you complete tasks in Mechanical Turk? Please check any of the following that applies:",[])
+    q10.addOption("Fruitful way to spend free time and get some cash (e.g., instead of watching TV).")
+    q10.addOption("For “primary” income purposes (e.g., gas, bills, groceries, credit cards).")
+    q10.addOption("For “secondary” income purposes, pocket change (for hobbies, gadgets, going out).")
+    q10.addOption("To kill time.")
+    q10.addOption("I find the tasks to be fun.")
+    q10.addOption("I am currently unemployed, or have only a part time job.")
+    #question 11
+    q11 = Question("dropdown", "Has the recession affected your decision to participate on MTurk?",[])
+    q11.addOption("Yes")
+    q11.addOption("No")
+    #question 12
+    q12 = Question("dropdown", "Has the recession affected your level of participation on MTurk?",[])
+    q12.addOption("Yes")
+    q12.addOption("No")      
+    #question 13
+    q13 = Question("radio", "For how long have you been working on Amazon Mechanical Turk?",[])
+    q13.addOption("< 6 mos.")
+    q13.addOption("6mos – 1yr")
+    q13.addOption("1-2yrs")
+    q13.addOption("2-3yrs")
+    q13.addOption("3-5yrs")
+    q13.addOption("5-7yrs")
+    q13.addOption("7-9yrs")
+    q13.addOption("9-15yrs")
+    q13.addOption("15+")
+    #question 14
+    q14 = Question("dropdown","How much do you earn per week on Mechanical Turk?",[])
+    q14.addOption("Less than $1 per week")
+    q14.addOption("$1-$5 per week.")
+    q14.addOption("$5-$10 per week.")
+    q14.addOption("$10-$20 per week.")
+    q14.addOption("$20-$50 per week.")
+    q14.addOption("$50-$100 per week.")
+    q14.addOption("$100-$200 per week.")
+    q14.addOption("$200-$500 per week.")
+    q14.addOption("More than $500 per week.")
+    #question 15
+    q15 = Question("dropdown","How much time do you spend per week on Mechanical Turk?",[])
+    q15.addOption("Less than 1 hour per week.")
+    q15.addOption("1-2 hours per week.")
+    q15.addOption("2-4 hours per week.")
+    q15.addOption("4-8 hours per week.")
+    q15.addOption("8-20 hours per week.")
+    q15.addOption("20-40 hours per week.")
+    q15.addOption("More than 40 hours per week.")
+    #question 16
+    q16 = Question("dropdown","How many HITs do you complete per week on Mechanical Turk?",[])
+    q16.addOption("Less than 1 HIT per week.")
+    q16.addOption("1-5 HITs per week.")
+    q16.addOption("5-10 HITs per week.")
+    q15.addOption("10-20 HITs per week.")
+    q16.addOption("20-50 HITs per week.")
+    q16.addOption("50-100 HITs per week.")
+    q16.addOption("100-200 HITs per week.")
+    q16.addOption("200-500 HITs per week.")
+    q16.addOption("500-1000 HITs per week.")
+    q16.addOption("1000-5000 HITs per week.")
+    q16.addOption("More than 5000 HITs per week.")
+    
 
     block1 = Block([q1,q2,q3,q4,q5,q6,q7,q8,q9])
-
-    #print block1
-
-    survey = Survey([block1])
-
-    print survey.jsonize()
+    block3 = Block([q10,q11,q12,q13,q14,q15,q16])
+    survey = Survey([block1, block3])
+    print survey
+    #print survey.jsonize()
     
     
 if  __name__ =='__main__':
