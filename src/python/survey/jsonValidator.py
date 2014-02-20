0d4b2feff3d65e515ad0d2d6fad3c569cd12f5a9
Molly K. McMahon <wester3450@aim.com>
2014-02-20 18:35:43 -0500

    pulled from untested, made temporary copy of json schema to put in python folder, fixed escape character issue in schema

--- src/python/survey/jsonValidator.py
+++ src/python/survey/jsonValidator.py
@@ -1,16 +1,18 @@
 from jsonschema import validate
+import json
 
 def validateJSON(schemaFile, jsonFile):
     s = open(schemaFile)
-    schema = s.read()
-    #print schema;
+    schemaText = s.read();
+    print schemaText
+    dataform = str(schemaText).strip("'<>()[]\"` ").replace('\'', '\"')
+    schema = json.loads(dataform);
     s.close;
     j = open(jsonFile, "r")
-    json = j.read()
+    jsonText = j.read()
     #print json
     j.close()
-    schema = eval(schema)
-    validate(json, )
+    validate(jsonText, schema)
 
 def main():
     validateJSON("survey-temp.JSON","survey1.JSON");
