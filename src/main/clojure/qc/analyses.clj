206814a13f8f5611cb9bc889764673632ddf256c
etosch <etosch@gmail.com>
2014-04-20 17:23:44 -0400

    pfft

--- src/main/clojure/qc/analyses.clj
+++ src/main/clojure/qc/analyses.clj
@@ -6,21 +6,19 @@
     (:import (java.util List)
              (org.apache.log4j Logger)
              (org.apache.commons.math3.stat.inference MannWhitneyUTest)
-             (survey Block$BranchParadigm SurveyResponse$OptTuple))
+             (survey Block$BranchParadigm SurveyResponse$OptTuple)
+             (csv CSVLexer))
     (:import (survey Survey Question Component SurveyResponse SurveyResponse$QuestionResponse))
     (:require [incanter core stats]
-              [clojure.math.numeric-tower :as math])
+              [clojure.math.numeric-tower :as math]
+              [clojure.test :as test])
 )
 
 (def LOGGER (Logger/getLogger (str (ns-name *ns*))))
-(def srid 0)
-(def opts 1)
-(def indexSeen 2)
 
-(defn map-to-seq
-    [m]
-    (map #(vector % (m %)) (keys m))
-    )
+(defrecord Response [^String srid
+                     ^List opts
+                     ^Integer indexSeen])
 
 (defn make-ans-map
     "Takes each question and returns a map from questions to a list of question responses.
@@ -29,40 +27,32 @@
     (let [answers (for [^SurveyResponse sr surveyResponses]
                       (apply merge
                           (for [^SurveyResponse$QuestionResponse qr (.responses sr)]
-                                           {(.q qr) (list [(.srid sr)
-                                                        (map (fn [opt] (.c ^SurveyResponse$OptTuple opt))
-                                                              (.opts qr))
-                                                        (.indexSeen qr)])})))]
+                                           {(.q qr) (list (Response. (.srid sr)
+                                                                     (map (fn [opt] (.c ^SurveyResponse$OptTuple opt)) (.opts qr))
+                                                                     (.indexSeen qr)))}
+                          )
+                      )
+                  )]
         (reduce #(merge-with concat %1 %2) answers)))
 
-(defn- convertToOrdered
+(defn convertToOrdered
     [q]
-    (into {} (zipmap (map (fn [opt] (.getCid opt)) (.getOptListByIndex q))
-                     (range 1 (count (.getOptListByIndex q)))))
-    )
+    "Returns a map of cids (String) to integers for use in ordered data."
+    (into {} (zipmap (map (fn [opt] (.getCid opt)) (sort-by #(.getSourceRow %) (.getOptListByIndex q)))
+                     (range 1 (count (.options q)))))
+)
 
-(defn- getOrdered
+(defn getOrdered
+    "Returns an integer corresponding to the ranked order of the option."
     [q opt]
-    (let [m (convertToOrdered q)
-          retval (get m (.getCid opt))]
-        (println (str "retval: " retval (.getCid opt) m))
-        (assert (contains? m (.getCid opt)) 'foo)
-        retval
+    (let [m (convertToOrdered q)]
+        (assert (contains? m (.getCid opt)) (clojure.string/join "\n" (list opt q m)))
+        (get m (.getCid opt))
     )
 )
 
-(defn- makeContingencyTable
-    [^Question q1 ^Question q2 ansMap]
-        (->> (for [opt1 (.getOptListByIndex q1) opt2 (.getOptListByIndex q2)]
-             ;; count the number of people who answer both opt1 and opt2
-             (let [answeredOpt1 (set (map #(% srid) (flatten (filter #(= (% opts) opt1) (ansMap q1)))))
-                   answeredOpt2 (set (map #(% srid) (flatten (filter #(= (% opts) opt2) (ansMap q2)))))]
-                 (count (clojure.set/intersection answeredOpt1 answeredOpt2))))
-         (partition (count (.getOptListByIndex q1)))
-         (incanter.core/matrix))
-    )
-
 (defn find-first
+    ;; there used to be a find-first in seq-utils, but I don't know where this went in newer versions of clojure
     [pred coll]
     (cond (empty? coll) nil
           (pred (first coll)) (first coll)
@@ -77,7 +67,7 @@
            l2sorted '()]
         (if (empty? pointer)
             [l1sorted l2sorted]
-            (let [matched (find-first #(= (% srid) ((first pointer) srid)) l2)]
+            (let [matched (find-first #(= (:srid %) (:srid (first pointer))) l2)]
                 (if (nil? matched)
                     (recur (rest pointer) l1sorted l2sorted)
                     (recur (rest pointer) (cons (first pointer) l1sorted) (cons matched l2sorted))
@@ -87,106 +77,109 @@
         )
     )
 
+(defn mann-whitney
+    [x y]
+    (try
+        (let [ mw (MannWhitneyUTest.) ]
+            { :stat 'mann-whitney
+              :val { :U (.mannWhitneyU mw x y)
+                     :p-value (.mannWhitneyUTest mw x y)
+                     }
+              }
+            )
+        (catch Exception e (.warn LOGGER (.getMessage e)))
+        )
+    )
+
+(defn chi-squared
+    [tab]
+    (try
+        { :stat 'chi-squared
+          :val (incanter.stats/chisq-test :table tab)
+        }
+        (catch Exception e (.warn LOGGER (.getMessage e)))
+        )
+    )
+
+(defn spearmans-rho
+    [l1 l2]
+    (try
+        (incanter.stats/spearmans-rho l1 l2)
+        (catch Exception e (.warn LOGGER (.getMessage e)))
+        )
+    )
+
 (defn correlation
     [surveyResponses ^Survey survey]
     (let [ansMap (make-ans-map surveyResponses)]
-        (assert (every? identity (flatten (for [k (keys ansMap)]
-                                              (map (fn [optlist]
-                                                       (map #(contains? (set (keys (.options k))) (% opts))
-                                                            optlist)
-                                                       )
-                                                   (map opts (ansMap k))
-                                                   )
-                                              )
-                                          )
-                        )
-                )
         (for [^Question q1 (.questions survey) ^Question q2 (.questions survey)]
             (let [[ans1 ans2] (align-by-srid (ansMap q1) (ansMap q2))]
                 ;; make sure order is retained
-                (assert (every? identity (map #(= (%1 srid) (%2 srid)) ans1 ans2)))
+                (assert (every? identity (map #(= (:srid %1) (:srid %2)) ans1 ans2)))
                 { :q1&ct [q1 (count ans1)]
                   :q2&ct [q2 (count ans2)]
-                  :corr (if (and (.exclusive q1) (.exclusive q2))
-                            (try
-                                (if (and (.ordered q1) (.ordered q2))
-                                    (let [n (min (count ans1) (count ans2))]
-                                        { :coeff 'rho
-                                          :val (incanter.stats/spearmans-rho (map #(getOrdered q1 (first %)) (take n ans1))
-                                                                             (map #(getOrdered q2 (first %)) (take n ans2)))
-                                        }
-                                    )
-                                    (let [tab (makeContingencyTable q1 q2 ansMap)
-                                          {X-sq :X-sq :as data} (incanter.stats/chisq-test :table tab)
-                                          N (reduce + (flatten tab))
-                                          k (min (tab :cols) (tab :rows))]
-                                            (merge data {:coeff 'V
-                                                         :val (math/sqrt (/ X-sq (* N (dec k))))
-                                                        }
-                                            )
-                                    )
+                  :corr (when (and (.exclusive q1) (.exclusive q2))
+                            (if (and (.ordered q1) (.ordered q2))
+                                (let [n (min (count ans1) (count ans2))]
+                                    { :coeff 'rho
+                                      :val (spearmans-rho (map #(getOrdered q1 (first %)) (take n ans1))
+                                                          (map #(getOrdered q2 (first %)) (take n ans2)))
+                                    })
+                                (let [tab (->> (for [opt1 (.getOptListByIndex q1) opt2 (.getOptListByIndex q2)]
+                                                    ;; count the number of people who answer both opt1 and opt2
+                                                    (let [answeredOpt1 (set (map #(:srid %) (flatten (filter #(= (:opts %) opt1) (ansMap q1)))))
+                                                          answeredOpt2 (set (map #(:srid %) (flatten (filter #(= (:opts %) opt2) (ansMap q2)))))]
+                                                        (count (clojure.set/intersection answeredOpt1 answeredOpt2))))
+                                                (partition (count (.getOptListByIndex q1)))
+                                                (incanter.core/matrix))
+
+                                      {X-sq :val :as data} (chi-squared tab)
+                                      N (reduce + (flatten tab))
+                                      k (apply min (incanter.core/dim tab))]
+                                    (when (> N 0) (> k 1)
+                                        (merge data {:coeff 'V
+                                                     :val (math/sqrt (/ X-sq (* N (dec k))))
+                                                    })
+                                        )
                                 )
-                            (catch Exception e
-                                (.warn LOGGER (.getMessage e)))))
-                      }
-                )
+                            )
+                        )
+                }
+            )
         )
     )
 )
 
-(defn difference-of-distr
-    [^Question q1 ^Question q2 x y ansMap]
-    (if (.exclusive q1)
-        (if (.ordered q1)
-            (let [ mw (MannWhitneyUTest.) ]
-                { :stat 'mann-whitney
-                  :val { :U (.mannWhitneyU mw x y)
-                         :p-value (.mannWhitneyUTest mw x y)
-                         }
-                  }
-                )
-            (let [tab (makeContingencyTable q1 q2 ansMap)]
-                { :stat 'chi-squared
-                  :val (incanter.stats/chisq-test :table tab)
-                  }
-                )
-            )
-        )
-    )
 
 (defn orderBias
     [surveyResponses ^Survey survey]
     (let [ansMap (make-ans-map surveyResponses)]
-        (assert (every? identity (flatten (for [k (keys ansMap)]
-                                              (map (fn [optlist]
-                                                       (map #(contains? (set (keys (.options k))) (.getCid %))
-                                                            optlist)
-                                                       )
-                                                   (map #(% opts) (ansMap k))
-                                                   )
-                                              )
-                                          )
-                        )
-                )
         (remove nil?
             (for [^Question q1 (.questions survey) ^Question q2 (.questions survey)]
-                (when (not= q1 q2)
+                (when (and (.exclusive q1) (not (.freetext q1)))
                     (let [[q1ans q2ans] (align-by-srid (ansMap q1) (ansMap q2))
                           tmp (map vector q1ans q2ans)
-                          q1answersq1first (filter (fn [pair] (< ((pair 0) indexSeen) ((pair 1) indexSeen))) tmp)
-                          q1answersq2first (filter (fn [pair] (> ((pair 0) indexSeen) ((pair 1) indexSeen))) tmp)
+                          q1answersq1first (map first (filter (fn [pair] (< (:indexSeen (pair 0)) (:indexSeen (pair 1)))) tmp))
+                          q1answersq2first (map first (filter (fn [pair] (> (:indexSeen (pair 0)) (:indexSeen (pair 1)))) tmp))
                          ]
-                        (println q1answersq1first)
-                          (let [  x (into-array Double/TYPE (map #(double (getOrdered q1 (first (% opts)))) (map first q1answersq1first)))
-                                  y (into-array Double/TYPE (map #(double (getOrdered q1 (first (% opts)))) (map first q1answersq2first)))
-                                  ]
-                                      { :q1 q1
-                                        :q2 q2
-                                        :numq1First (count q1answersq1first)
-                                        :numq2First (count q1answersq2first)
-                                        :order (difference-of-distr q1 q2 x y ansMap)
-                                      }
-                          )
+                        { :q1 q1
+                          :q2 q2
+                          :numq1First (count q1answersq1first)
+                          :numq2First (count q1answersq2first)
+                          :order (if (.ordered q1)
+                                     (let [x (into-array Double/TYPE (map #(double (getOrdered q1 (first (:opts %)))) q1answersq1first))
+                                           y (into-array Double/TYPE (map #(double (getOrdered q1 (first (:opts %)))) q1answersq2first))
+                                          ]
+                                         (mann-whitney x y))
+                                     (letfn [(getCountsForContingencyTab [lst] (map (fn [^Component opt]
+                                                                                        (count (filter (fn [^Response r]
+                                                                                                           (= (.getCid (first (:opts r)))
+                                                                                                              (.getCid opt)))
+                                                                                                       lst)))
+                                                                                    (.getOptListByIndex q1)))]
+                                         (chi-squared (incanter.core/matrix (list (getCountsForContingencyTab q1answersq1first)
+                                                                                  (getCountsForContingencyTab q1answersq2first))))))
+                        }
                     )
                 )
             )
@@ -194,39 +187,39 @@
     )
 )
 
-(defn get-questions-with-variants
-    [^Survey survey]
-    (loop [blocks (vals (.blocks survey))
-           retval '()]
-        (if (empty? blocks)
-            retval
-            (recur (rest blocks)
-                   (if (= (.branchParadigm (first blocks)) Block$BranchParadigm/SAMPLE)
-                       (cons (.questions (first blocks)) retval)
-                       retval)
-                   )
-            )
-    )
-)
-
+;(defn get-questions-with-variants
+;    [^Survey survey]
+;    (loop [blocks (vals (.blocks survey))
+;           retval '()]
+;        (if (empty? blocks)
+;            retval
+;            (recur (rest blocks)
+;                   (if (= (.branchParadigm (first blocks)) Block$BranchParadigm/SAMPLE)
+;                       (cons (.questions (first blocks)) retval)
+;                       retval)
+;                   )
+;            )
+;    )
+;)
+;
 (defn wordingBias
     [surveyResponses ^Survey survey]
-    (let [ansMap (make-ans-map surveyResponses)
-          variantList (get-questions-with-variants survey)]
-        (for [variants variantList]
-            (for [^Question q1 variants ^Question q2 variants]
-                (let [q1ans (ansMap q1)
-                      q2ans (ansMap q2)
-                      x (into-array Double/TYPE (map #(double (getOrdered q1 (% opts))) q1ans))
-                      y (into-array Double/TYPE (map #(double (getOrdered q2 (% opts))) q2ans))]
-                    { :q1&ct [q1 (count q1ans)]
-                      :q2&ct [q2 (count q2ans)]
-                      :order (difference-of-distr q1 q2 x y ansMap)
-                      }
-                    )
-                )
-            )
-        )
+;    (let [ansMap (make-ans-map surveyResponses)
+;          variantList (get-questions-with-variants survey)]
+;        (for [variants variantList]
+;            (for [^Question q1 variants ^Question q2 variants]
+;                (let [q1ans (ansMap q1)
+;                      q2ans (ansMap q2)
+;                      x (into-array Double/TYPE (map #(double (getOrdered q1 (:opts %))) q1ans))
+;                      y (into-array Double/TYPE (map #(double (getOrdered q2 (:opts %))) q2ans))]
+;                    { :q1&ct [q1 (count q1ans)]
+;                      :q2&ct [q2 (count q2ans)]
+;                      :order (difference-of-distr q1 q2 x y ansMap)
+;                      }
+;                    )
+;                )
+;            )
+;        )
     )
 
 (defn -getCorrelations
--- src/test/clojure/testAnalyses.clj
+++ src/test/clojure/testAnalyses.clj
@@ -1,44 +1,95 @@
 (ns testAnalyses
-    (:import (qc RandomRespondent RandomRespondent$AdversaryType analyses)
+    (:import (qc RandomRespondent RandomRespondent$AdversaryType)
              (csv CSVLexer CSVParser)
-             (survey Survey Question))
+             (survey Survey Question Component)
+             (org.apache.log4j Logger))
     (:use clojure.test)
     (:use testLog)
+    (:require (qc analyses))
     )
 
+(def numResponses 10)
 (def correlationThreshhold 0.5)
 (def alpha 0.05)
 (def falseCorrelations (atom 0))
+(def totalTested (atom 0))
 (def falseOrderBias (atom 0))
 
-(defn- getRandomSurveyResponses
+(defn getRandomSurveyResponses
     [survey n]
     (clojure.core/repeatedly n #(RandomRespondent. survey RandomRespondent$AdversaryType/UNIFORM))
 )
 
-(defn- makeSurvey
+(defn makeSurvey
     [filename sep]
     (->> (CSVLexer. filename sep)
          (CSVParser.)
          (.parse))
     )
 
-(defn- generate1000RandomResponses
+(defn generateNRandomResponses
     [survey]
     (map (fn [^RandomRespondent rr] (.response rr))
-         (getRandomSurveyResponses survey 1000))
+         (getRandomSurveyResponses survey numResponses))
     )
 
-(deftest correlation
+
+(deftest test-answer-map
     (doseq [[filename sep] tests]
-        (println filename)
+        (println "test-answer-map" filename)
+        (let [ansMap (->> (makeSurvey filename sep)
+                          (generateNRandomResponses)
+                          (qc.analyses/make-ans-map))]
+            (doseq [k (keys ansMap)]
+                (when-not (.freetext k)
+                    (doseq [^qc.analyses/Response r (ansMap k)]
+                        (is (contains? (set (map #(.getCid %) (.getOptListByIndex k))) (.getCid (first (:opts r)))))
+                        )
+                    )
+                )
+            )
+        )
+    )
+
+(deftest test-ordered
+    (doseq [[filename sep] tests]
+        (println "test-ordered" filename)
+        (let [^Survey survey (makeSurvey filename sep)]
+            (doseq [q (.questions survey)]
+                (println (.freetext q))
+                (System/exit 1)
+                (when-not (.freetext q)
+                    (doseq [opt (vals (.options q))]
+                        (is (= (qc.analyses/getOrdered q opt)
+                               (-> (sort-by #(.getSourceRow %) (vals (.options q)))
+                                   (.indexOf (.getCid opt))
+                                   (inc))))
+                        )
+                    )
+                )
+            )
+        )
+    )
+
+(deftest test-align-by-srid
+    (doseq [[filename sep] tests]
+        (println "test-align-by-srid" filename)
+        (let [^Survey survey (makeSurvey filename sep)]
+            ;; is this necessary?
+            )
+        )
+    )
+
+(deftest test-correlation
+    (doseq [[filename sep] tests]
+        (println "test-correlation:" filename)
         (let [^Survey survey (makeSurvey filename sep)
-              responses (generate1000RandomResponses survey)
+              responses (generateNRandomResponses survey)
               correlations (qc.analyses/correlation responses survey)]
             (doseq [{[^Question q1 ct1] :q1&ct [^Question q2 ct2] :q2&ct {coeff :coeff val :val} :corr} correlations]
                 (when (and coeff val)
                     (if (= q1 q2)
-                        (assert (= 1.0 val))
+                        (is (= 1.0 val))
                         (when (> val correlationThreshhold)
                             (.warn LOGGER (format (str "Random respondent generated a correlation %s = %f > %f for questions"
                                                         "%s (quid : %s, ct : %d, numOpts : %d) and "
@@ -49,22 +100,25 @@
                             (swap! falseCorrelations inc)
                         )
                     )
+                    (swap! totalTested inc)
                 )
             )
             (printf "Number false correlations for %s: %d\n" filename @falseCorrelations)
+            (printf "\tTotal comparisons : %d\n" @totalTested)
         )
+        (reset! totalTested 0)
         (reset! falseCorrelations 0)
     )
 )
 
-(deftest orderBias
+(deftest test-orderBias
     (doseq [[filename sep] tests]
         (println filename)
         (let [^Survey survey (makeSurvey filename sep)
-              responses (generate1000RandomResponses survey)
+              responses (generateNRandomResponses survey)
               ob (qc.analyses/orderBias responses survey)]
             (doseq [{q1 :q1 q2 :q2 num1 :numq1First num2 :numq2First {stat :stat val :val} :order} ob]
-                (when (< val alpha)
+                (when (and val (< val alpha))
                     (.warn LOGGER (format (str "Random respondent generated order bias (%s = %f) between"
                                                "%s (quid : %s, ct : %s, numOpts : %d and"
                                                "%s (quid : %s, ct : %s, numOpts : %d\n")
@@ -80,11 +134,11 @@
     (reset! falseOrderBias 0)
     )
 
-(deftest variantBias
+(deftest test-variantBias
     (doseq [[filename sep] tests]
         (println filename)
         (let [^Survey survey (makeSurvey filename sep)
-              responses (generate1000RandomResponses survey)
+              responses (generateNRandomResponses survey)
               variantsList (qc.analyses/wordingBias responses survey)]
             )
         )
