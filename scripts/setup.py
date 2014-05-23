751f6049854737884c3920ce677e94175f129fe9
Daniel Barowy <dbarowy@cs.umass.edu>
2014-05-23 17:25:00 -0400

    Reverting scripts/setup.py since this is used *after* making a release; resources are already in the right place in that case.

--- scripts/setup.py
+++ scripts/setup.py
@@ -2,16 +2,15 @@
 # regardless of OS, want to create a surveyman home dir and move
 # params.properties tere
 import os
-import shutil
 
 home = os.path.expanduser("~")
-resources = "src" + os.sep + "main" + os.sep + "resources"
 surveyman = home + os.sep + "surveyman"
 params = "params.properties"
 customjs = "custom.js"
 customcss = "custom.css"
 if not os.path.exists(surveyman) :
     os.mkdir(surveyman)
-shutil.copyfile(resources + os.sep + params, surveyman + os.sep + params)
-shutil.copyfile(resources + os.sep + customjs, surveyman + os.sep + customjs)
-shutil.copyfile(resources + os.sep + customcss, surveyman + os.sep + customcss)
+os.rename(params, surveyman + os.sep + params)
+os.rename(customjs, surveyman + os.sep + customjs)
+os.rename(customcss, surveyman + os.sep + customcss)
+
