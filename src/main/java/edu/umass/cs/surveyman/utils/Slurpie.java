306089e85faf1360e54e67b7394c6c9de4e5dc55
John Foley <jfoley@cs.umass.edu>
2015-06-25 13:46:15 -0400

    Refactor Slurpie so I can use it on a BufferedReader or two

--- src/main/java/edu/umass/cs/surveyman/utils/Slurpie.java
+++ src/main/java/edu/umass/cs/surveyman/utils/Slurpie.java
@@ -9,34 +9,66 @@ import java.net.URL;
 public class Slurpie {
     // convenience class to slurp in a whole file
 
+    /**
+     * Read the contents of a file.
+     * @param filename file or URL.
+     * @return contents as a String.
+     * @throws IOException
+     */
     public static String slurp(String filename) throws IOException {
         return slurp(filename, Integer.MAX_VALUE);
     }
 
-    public static String slurp(String filename, int numChars) throws IOException {
+    /**
+     * Get a reader for a file or URL.
+     * @param filename file or URL.
+     * @return a BufferedReader, make sure to close it.
+     * @throws IOException
+     */
+    public static BufferedReader getReader(String filename) throws IOException {
         URL resource = Slurpie.class.getClassLoader().getResource(filename);
         BufferedReader br = null;
-        try {
-            if (resource == null) {
-                try {
-                    br = new BufferedReader(new FileReader(filename));
-                } catch (FileNotFoundException fe) {
-                    br = new BufferedReader(new InputStreamReader(new URL(filename).openStream()));
-                }
-            } else br = new BufferedReader(new InputStreamReader(resource.openStream()));
-            StringBuilder s = new StringBuilder();
-            char[] buf = new char[1024 * 1024];
-            for (int totalCharsRead = 0; totalCharsRead < numChars; ) {
-                int charsRead = br.read(buf);
-                if (charsRead == -1)
-                    break;
-                s.append(buf, 0, charsRead);
-                totalCharsRead += buf.length;
+        if (resource == null) {
+            try {
+                br = new BufferedReader(new FileReader(filename));
+            } catch (FileNotFoundException fe) {
+                br = new BufferedReader(new InputStreamReader(new URL(filename).openStream()));
             }
-            return s.toString();
-        } finally {
-            if (br != null) br.close();
+        } else br = new BufferedReader(new InputStreamReader(resource.openStream()));
+        return br;
+    }
+
+    /**
+     * Slurp in a filename up to a maximimum number of characters.
+     * @param filename file name or url.
+     * @param numChars maximum size in characters.
+     * @return a string of up to numChars.
+     * @throws IOException
+     */
+    public static String slurp(String filename, int numChars) throws IOException {
+        try (BufferedReader br = getReader(filename)) {
+            return slurp(br, numChars);
         }
+    }
+
+    /**
+     * Slurp from a BufferedReader up to roughly numChars characters of data.
+     * @param br the buffered reader.
+     * @param numChars the number of characters to read.
+     * @return the data as a string.
+     * @throws IOException
+     */
+    public static String slurp(BufferedReader br, int numChars) throws IOException {
+        StringBuilder s = new StringBuilder();
+        char[] buf = new char[1024 * 1024];
+        for (int totalCharsRead = 0; totalCharsRead < numChars; ) {
+            int charsRead = br.read(buf);
+            if (charsRead == -1)
+                break;
+            s.append(buf, 0, charsRead);
+            totalCharsRead += buf.length;
+        }
+        return s.toString();
     }
 
     public static String slurp(String filename, boolean ignoreErr) throws IOException{
@@ -47,6 +79,5 @@ public class Slurpie {
             if (!ignoreErr) throw io;
         }
         return retval;
-
     }
 }
